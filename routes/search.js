9098d6d36ea2172b34b8e0d61d96cf444e89e192
Polina Skrobot <polinaskrobot@Polinas-iMac.local>
2021-10-27 20:38:35 -0700

    More queries

--- routes/search.js
+++ routes/search.js
@@ -4,13 +4,19 @@ const router = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    db.query(`SELECT users.*, array_agg( DISTINCT '[' || orders.id || ',' || orders.parent_id || ',' || orders.hours ||']') AS orders,
+    db.query(`SELECT users.*, json_agg( DISTINCT orders.*) AS orders,
     json_agg(DISTINCT prices.*) AS prices,
-    json_agg(DISTINCT availability.*) AS availability
+    json_agg(DISTINCT availability.*) AS availability,
+    array_agg(DISTINCT age_groups.age_group::TEXT) AS age_group, 
+    array_agg(DISTINCT languages.language::TEXT) AS language
      FROM users
+      JOIN users_languages ON users.id = users_languages.user_id
+      JOIN languages ON users_languages.language_id = languages.id
     inner JOIN orders ON users.id = orders.sitter_id
     inner JOIN prices ON users.id = prices.user_id
     inner JOIN availability ON users.id = availability.sitter_id
+    JOIN users_age_groups ON users.id = users_age_groups.user_id
+    JOIN age_groups ON users_age_groups.age_group_id = age_groups.id
     WHERE babysitter=true GROUP BY users.id`)
       .then((data)=>{
         res.cookie("user_id", 1);
