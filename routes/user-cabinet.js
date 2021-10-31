ce8c059cd2750622eab13716a37e0c07e0fdfe37
Polina Skrobot <polinaskrobot@Polinas-iMac.local>
2021-10-30 22:15:11 -0700

    Response with sitter name and photo for /user-cabinet

--- routes/user-cabinet.js
+++ routes/user-cabinet.js
@@ -3,11 +3,15 @@ const router = express.Router();
 
 module.exports=(db)=>{
   router.get("/", (req, res) => {
-    //console.log("req from user cab", req);
-    db.query(`SELECT *
-    FROM orders
-    WHERE parent_id=1;`)
-      .then((data)=>{
+    //console.log("req from user cab", req);   
+    
+      db.query(`SELECT orders.*, users.first_name as sitter_name, users.photo as sitter_photo
+      FROM orders
+      LEFT JOIN users ON orders.sitter_id = users.id
+      WHERE orders.parent_id = 1
+      ORDER BY orders.id;`)
+
+        .then((data)=>{
         //console.log("get user-cabinet", data.rows)
         res.json(data.rows);
       });  
