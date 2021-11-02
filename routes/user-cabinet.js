fc1f4924444f8e8a627cd30f31b1736861373fb0
Polina Skrobot <polinaskrobot@Polinas-iMac.local>
2021-11-02 10:24:06 -0700

    Added comments to orders query for user-cabinet

--- routes/user-cabinet.js
+++ routes/user-cabinet.js
@@ -5,9 +5,11 @@ module.exports=(db)=>{
   router.get("/", (req, res) => {
     //console.log("req from user cab", req);   
     
-      db.query(`SELECT orders.*, users.first_name as sitter_name, users.photo as sitter_photo
+      db.query(`SELECT orders.*, users.first_name as sitter_name, users.photo as sitter_photo,
+       reviews_for_sitter.comment as comment, reviews_for_sitter.rate as rate
       FROM orders
       LEFT JOIN users ON orders.sitter_id = users.id
+      LEFT JOIN reviews_for_sitter ON orders.id = reviews_for_sitter.order_id
       WHERE orders.parent_id = 1
       ORDER BY orders.id;`)
 
